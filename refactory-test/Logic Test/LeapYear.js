3472fa6863421a5ac388aa79293e3bd9cba87f8d
Ardhian <ardhian@Ardhians-MacBook-Pro.local>
2021-06-06 08:12:20 +0700

    add condition year leap

--- refactory-test/Logic Test/LeapYear.js
+++ refactory-test/Logic Test/LeapYear.js
@@ -8,17 +8,21 @@ function LeapYear(year1, year2) {
       year1 += 1;
     }
   }
-  // if ((year1 % 4 === 0 && year1 % 100 !== 0) || year1 % 400 === 0) {
   if (year1 % 4 === 0) {
     start = year1;
   }
   console.log("Start Year Leap : ", start);
 
-  for (temp = start; temp + 4 < year2; start) {
-    temp += add;
-    years.push(temp);
+  for (temp = start; temp < year2; start) {
+    if ((temp % 4 === 0 && temp % 100 !== 0) || temp % 400 === 0) {
+      years.push(temp);
+      temp += add;
+    } else {
+      temp += add;
+    }
   }
   console.log("Year Leap : ", years);
 }
 
+LeapYear(1890, 1920);
 LeapYear(1995, 2021);
